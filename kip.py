afada756471fa2ff63c0930a2dfcce8f3aea1e33
Chasewb91 <chasewb91@gmail.com>
2022-07-19 15:27:26 -0500

    blank slate bot added

---
+++ kip.py
@@ -0,0 +1,19 @@
+import discord #imports discord.py module, allowing use of their API.
+import os #for loading discord token from .env file
+from discord.ext import commands, tasks 
+from dotenv import load_dotenv #for loading discord token from .env file
+
+load_dotenv()
+token=os.getenv('discord_token') #pulls the discord token from a .env file -- 
+
+# For the bot to launch with this set up, you must have a .env file with the following, excluding quotations "discord_token=DiscordBotTokenGoesHere" 
+
+client = commands.Bot(command_prefix='!', activity=discord.Game(name="Scrabble")) 
+
+@client.command()
+async def ping(ctx):
+  await ctx.channel.send("pong")
+    
+client.run(token)
+
+    
